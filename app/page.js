fa0060dc8bcd3f9c387957c2909580562ac0e274
anonymous298 <tackletalha@gmail.com>
2025-10-14 23:07:07 +0500

    main page.js updated

--- app/page.js
+++ app/page.js
@@ -1,10 +1,21 @@
+import BlogsHero from "@/components/BlogsHero";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-       
+       {/* Navbar component */}
+       <Navbar />
+
+      {/* Main blog display component */}
+      <BlogsHero />
+
+      <Footer />
     </div>
   );
 }
+
+
  
