4fc14834e76dfee2ac7a9990081e320114b2346e
anonymous298 <tackletalha@gmail.com>
2025-10-18 11:27:21 +0500

    layout for admin panel chnage

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,11 +1,17 @@
+import Navbar from "@/components/AdminComponents/Navbar";
 import SideBar from "@/components/AdminComponents/SideBar";
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function Layout({children}) {
     return (
         <div className="flex">
+            <ToastContainer theme="dark" />
+            
             <SideBar />
 
-            <div className="w-[70vw] min-h-screen">
+            <div className="w-[74vw] min-h-screen">
+                <Navbar />
                 {children}
             </div>
         </div>
