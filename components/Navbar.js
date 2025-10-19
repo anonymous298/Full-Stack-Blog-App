5307fa98cee46bd91c6272acafd04c9ea461844a
anonymous298 <tackletalha@gmail.com>
2025-10-19 16:38:49 +0500

    changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
           </Link>
 
           <div>
-            <button className="border-2 py-2 px-6 max-md:px-5 max-sm:py-1 max-sm:px-3 font-semibold shadow-[-7px_7px_0px_#000] transform transition-all hover:scale-105 cursor-pointer flex items-center gap-1">Get started <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="#000000" viewBox="0 0 256 256"><path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path></svg></button>
+            <Link href={'/admin'} className="border-2 py-2 px-6 max-md:px-5 max-sm:py-1 max-sm:px-3 font-semibold shadow-[-7px_7px_0px_#000] transform transition-all hover:scale-105 cursor-pointer flex items-center gap-1">Get started <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="#000000" viewBox="0 0 256 256"><path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path></svg></Link>
           </div>
 
           
